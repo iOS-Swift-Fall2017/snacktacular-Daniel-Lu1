0439fabd2a7a35cba66ce747d9cc09157be25524
ludg <ludg@bc.edu>
2017-11-29 15:19:00 -0500

    done

--- Snacktacular/DetailViewController.swift
+++ Snacktacular/DetailViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 import CoreLocation
+import MapKit
+import GooglePlaces
 
 class DetailViewController: UIViewController {
     
     @IBOutlet weak var placeNameLabel: UITextField!
     @IBOutlet weak var addressLabel: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
     
     var placeData: PlaceData?
+    var locationManager: CLLocationManager!
+    var currentLocation: CLLocation!
+    var regionRadius = 1000.0
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
@@ -22,18 +28,44 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+        
+        mapView.delegate = self
         if let placeData = placeData {
-            placeNameLabel.text = placeData.placeName
-            addressLabel.text = placeData.address
+            centerMap(mapLocation: placeData.coordinate, regionRadius: regionRadius)
+            mapView.removeAnnotations(mapView.annotations)
+            mapView.addAnnotation(placeData)
+            mapView.selectAnnotation(placeData, animated: true)
+            updateUserInterface()
         } else {
-            placeData = PlaceData(placeName: "", address: "", postingUserID: "", coordinate: CLLocationCoordinate2D())
+            placeData = PlaceData(placeName: "", address: "", postingUserID: "", coordinate: CLLocationCoordinate2D(), placeDocumentID: "")
+            getLocation()
         }
+    }
+    
+    func updateUserInterface() {
+        placeNameLabel.text = placeData!.placeName
+        addressLabel.text = placeData!.address
+    }
+    
+    func centerMap(mapLocation: CLLocationCoordinate2D, regionRadius: CLLocationDistance) {
+        let region = MKCoordinateRegionMakeWithDistance(mapLocation, regionRadius, regionRadius)
+        mapView.setRegion(region, animated: true)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         placeData?.placeName = placeNameLabel.text!
         placeData?.address = addressLabel.text!
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
     }
 
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
@@ -46,5 +78,124 @@ class DetailViewController: UIViewController {
     }
     
     @IBAction func lookupButtonPressed(_ sender: UIBarButtonItem) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        present(autocompleteController, animated: true, completion: nil)
     }
 }
+
+
+extension DetailViewController: CLLocationManagerDelegate {
+    
+    func getLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+    }
+    
+    func handleLocationAuthorizationStatus(status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.requestLocation()
+        case .denied:
+            print("No location to show")
+        case .restricted:
+            print("Access Denied")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        handleLocationAuthorizationStatus(status: status)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let geoCoder = CLGeocoder()
+        currentLocation = locations.last
+        let currentLatitude = currentLocation.coordinate.latitude
+        let currentLongitude = currentLocation.coordinate.longitude
+        
+        geoCoder.reverseGeocodeLocation(currentLocation, completionHandler:
+            {placemarks, error in
+                if placemarks != nil {
+                    let placemark = placemarks?.last
+                    self.placeData?.placeName = (placemark?.name)!
+                    self.placeData?.address = placemark?.thoroughfare ?? "unknown"
+                    self.placeData?.coordinate = CLLocationCoordinate2D(latitude: currentLatitude, longitude: currentLongitude)
+                    self.centerMap(mapLocation: (self.placeData?.coordinate)!, regionRadius: self.regionRadius)
+                    self.mapView.addAnnotation(self.placeData!)
+                    self.mapView.selectAnnotation(self.placeData!, animated: true)
+                    self.updateUserInterface()
+                } else {
+                    print("Error retrieving place")
+                }
+        })
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Fail")
+    }
+}
+
+extension DetailViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "Marker"
+        var view: MKPinAnnotationView
+        if let dequedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView {
+            dequedView.annotation = annotation
+            view = dequedView
+        } else {
+            view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.rightCalloutAccessoryView = UIButton(type: .custom)
+        }
+        return view
+    }
+}
+
+extension DetailViewController: GMSAutocompleteViewControllerDelegate {
+    
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        placeData?.placeName = place.name
+        placeData?.coordinate = place.coordinate
+        placeData?.address = place.formattedAddress ?? "unknown"
+        centerMap(mapLocation: (placeData?.coordinate)!, regionRadius: regionRadius)
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotation(self.placeData!)
+        mapView.selectAnnotation(self.placeData!, animated: true)
+        updateUserInterface()
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: ", error.localizedDescription)
+    }
+    
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
